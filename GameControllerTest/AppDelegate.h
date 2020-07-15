47ebcc58a3eca0e65f4219ba1433598b34639610
Connor <piefacevian@googlemail.com>
2020-07-15 12:58:25 +0100

    Initial Commit

---
+++ GameControllerTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameControllerTest
+//
+//  Created by connor aspinall on 15/07/2020.
+//  Copyright © 2020 connor aspinall. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GameControllerTest/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  GameControllerTest
+//
+//  Created by connor aspinall on 15/07/2020.
+//  Copyright © 2020 connor aspinall. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
